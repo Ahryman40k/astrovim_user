3a0570a31c2f49fc3421b6a2514688b54a3a416b
GEOFFREY BAUDIN <ahryman40k.gb@gmail.com>
2024-01-04 22:42:25 -0500

    community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,76 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+
+  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.color.modes-nvim" },
+  { import = "astrocommunity.color.tint-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.color.vim-highlighter" },
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.fluormachine-nvim", opts = {
+    theme = "delta",
+  } },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.comment-box-nvim" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.telescope-undo-nvim" },
+  { import = "astrocommunity.editing-support.text-case-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.editing-support.wildfire-nvim" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
+
+  { import = "astrocommunity.indent.indent-tools-nvim" },
+
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
+
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.vim-matchup" },
+
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.zig" },
+
+  { import = "astrocommunity.project.nvim-spectre" },
+
+  { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+
+  { import = "astrocommunity.test.neotest" },
+
+  { import = "astrocommunity.utility.noice-nvim" },
+  {
+    import = "astrocommunity.motion.harpoon",
+    opts = {
+      branch = "harpoon2",
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+    },
+  },
 }
